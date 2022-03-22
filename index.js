3872596f4bd8adfed7b57482c4f118c830affd25
James Bester <jamesthomasbester@gmail.com>
2022-03-22 19:02:33 +1100

    added via gitbash terminal

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("working")
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXNiZXN0ZXIiLCJhIjoiY2t1NmhnMTlkNWFtbDJ2bzJvaDV2MHN1MiJ9.Puw8sJNEnDYvULGp0odUhQ';
+
+const api = {
+    key: 'f40fdbed4ce6f49a21c55972b85d4f67',
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+const map = new mapboxgl.map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [144.9631, -37.8182],
+    zoom: 9
+});
